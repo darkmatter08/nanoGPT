fixed model_nn.py by fixing loss function and final normalization

bigram/model_nn.py
@@ -19,7 +19,6 @@ p(c = C | r) = matrix[r, C] / sum(matrix[r, :])
 import numpy as np
 import pickle 
 import os
-# from tqdm import tqdm
 
 import torch
 
@@ -30,18 +29,27 @@ MODEL_SAVEPATH: str = "bigram_model.pt"
 RELOAD_MODEL: bool = False
 
 # Sampling Config
-STARTING_CHAR: str = "T"
+STARTING_CHAR: str = "?"
 TOKENS_TO_SAMPLE: int = 1_000
 # TEMPERATURE: float = 0.0  # TODO: Figure out how to implment this
 
+# Training Config
 BATCH_SIZE: int = 1024
 TRAIN_ITERS: int = 10_000
 
+# Optimizer Config
+LR: float = 0.0001
+MOMENTUM: float = 0.90
 
-## Load dataset and override VOCAB_SIZE
-train_tokens = np.memmap("train.bin", dtype=np.int16, mode='r')
-test_tokens = np.memmap("test.bin", dtype=np.int16, mode='r')
-with open("meta.pkl", "rb") as f:
+EVAL: bool = True
+SAMPLE: bool = True
+
+## Load dataset
+prefix = "./"
+prefix = "/Users/jains/code/nanoGPT/data/world192"
+train_tokens = np.memmap(os.path.join(prefix, "train.bin"), dtype=np.int16, mode='r')
+test_tokens = np.memmap(os.path.join(prefix, "test.bin"), dtype=np.int16, mode='r')
+with open(os.path.join(prefix, "meta.pkl"), "rb") as f:
     meta = pickle.load(f)
 
 VOCAB_SIZE = len(set(train_tokens))
@@ -51,35 +59,24 @@ ctoi = lambda c: meta["ctoi_map"][c]
 itoc = lambda i: meta["itoc_map"][i]
 
 
-print(f"{train_tokens=} {test_tokens=}")
+def make_batch(test=False):
+    tokens = test_tokens if not test else train_tokens
+    # TODO: Shuffle within the batch
+    current_index = np.random.randint(0, len(tokens) - BATCH_SIZE - 1)
 
-# Convert to a tensor
-# TODO: Shuffle these batches.
-current_index: int = 0
-def make_batch():
-    global current_index
-
-    outputs = train_tokens[current_index+1: current_index+BATCH_SIZE+1]
-    inputs = train_tokens[current_index: current_index+len(outputs)]
+    outputs = tokens[current_index+1: current_index+BATCH_SIZE+1]
+    inputs = tokens[current_index: current_index+len(outputs)]
 
     output_batch = torch.tensor(outputs, dtype=torch.long)
     input_batch = torch.tensor(inputs, dtype=torch.long)
 
     assert input_batch.shape == output_batch.shape
 
-    current_index += BATCH_SIZE
-
-    if current_index >= len(train_tokens):
-        current_index = 0
-
     return (input_batch, output_batch)
-
-print(make_batch())
-(input_batch, output_batch) = make_batch()
 
 
 class Network(torch.nn.Module):
-    def __init__(self, h1_size=256, h2_size=512):
+    def __init__(self, h1_size=256, h2_size=2048):
         super(Network, self).__init__()
         self.h1_size = h1_size
         self.h2_size = h2_size
@@ -88,71 +85,94 @@ class Network(torch.nn.Module):
         self.layer2 = torch.nn.Linear(self.h1_size, self.h2_size)
         self.nonlinearity2 = torch.nn.ReLU()
         self.layer3 = torch.nn.Linear(self.h2_size, VOCAB_SIZE)
-        self.final = torch.nn.Softmax()
 
         torch.nn.init.kaiming_normal_(self.layer1.weight, mode='fan_in', nonlinearity='relu')
         torch.nn.init.kaiming_normal_(self.layer2.weight, mode='fan_in', nonlinearity='relu')
         torch.nn.init.kaiming_normal_(self.layer3.weight, mode='fan_in', nonlinearity='relu')
 
-        # T x B x C ??
-        # How does Linear deal with TxBxC?
-
     def forward(self, x):
         x = torch.nn.functional.one_hot(x, num_classes=VOCAB_SIZE)
         x = x.float()
-        # print(f"{x.shape=}")  # [16, 65] # B x vocab_size  # TODO: unsqueeze to get B x T=1 x vocab_size
         x = self.layer1(x)
-        # print(f"{x.shape=}")  # [16, 256]
         x = self.nonlinearity1(x)
         x = self.layer2(x)
-        # print(f"{x.shape=}")  # [16, 512]
         x = self.nonlinearity2(x)
         x = self.layer3(x)
-        # print(f"{x.shape=}")  # [16, 65]
-        x = self.final(x)
-        # print(f"{x.shape=}")  # [16, 65]  # Should be (B, C)
         return x
 
+
 model = Network()
-model.train(True)
+# Because we use CrossEntropyLoss, we don't apply a Softmax on the final output of the model. It should be un-normalized.
+loss_fn = torch.nn.CrossEntropyLoss()
+# loss_fn = torch.nn.NLLLoss()
+
 
 if RELOAD_MODEL and os.path.exists(MODEL_SAVEPATH):
     print(f"Reloading model from {MODEL_SAVEPATH}")
     model.load_state_dict(torch.load(MODEL_SAVEPATH))
+else:
+    model.train(True)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
-optimizer = torch.optim.Adam(model.parameters())
-loss_fn = torch.nn.CrossEntropyLoss()
+    for iter in range(TRAIN_ITERS):
+        batch_inputs, batch_outputs = make_batch()
 
-for iter in range(TRAIN_ITERS):
-    # get batch
-    batch_inputs, batch_outputs = make_batch()
-    
-    optimizer.zero_grad()
+        optimizer.zero_grad()
 
-    # forward
-    pred = model(batch_inputs)
-    # compute loss
-    # print(pred)
-    # print(pred.shape)
-    # print(batch_outputs)
-    # print(batch_outputs.shape)
-    loss = loss_fn(pred, batch_outputs)
-    # print(loss, loss.item())
-    # raise ValueError
-    # compute gradients
-    loss.backward()
-    # apply loss
-    optimizer.step()
+        pred = model(batch_inputs)
 
-    loss_scalar = loss.item()
-    if iter % 10 == 0:
-        print(f"{iter=} {loss_scalar=}")
+        loss = loss_fn(pred, batch_outputs)
+
+        # compute gradients
+        loss.backward()
+
+        # apply loss
+        optimizer.step()
+
+        loss_scalar = loss.item() / BATCH_SIZE
+        if iter % 10 == 0:
+            print(f"{iter=:05} {loss_scalar=:.6f}")
+
+    torch.save(model.state_dict(), MODEL_SAVEPATH)
+
 
 
 # WHY is it not training? is there something wrong in the loss function computation?
 
 
-## TODO: eval on test set
-        
+## Evals
+if EVAL:
+    model.train(False)
+    total_loss = 0
+    with torch.no_grad():
+        EVAL_ITERS = 100
+        for _ in range(EVAL_ITERS):  # TODO: figure out how to not shuffle
+            batch_inputs, batch_outputs = make_batch(test=True)
+            pred = model(batch_inputs)
+            loss = loss_fn(pred, batch_outputs)
 
-## TODO: Generative Sampling (Inference)
+            loss_scalar = loss.numpy().item()
+            total_loss += loss_scalar
+
+        total_loss /= (EVAL_ITERS * BATCH_SIZE)
+        print(f"Test Loss: {total_loss=:.5f}")
+
+
+## Generative Sampling (Inference)
+if SAMPLE:
+    model.train(False)
+
+    result = STARTING_CHAR
+    while len(result) - 1 < TOKENS_TO_SAMPLE:
+        last_token = ctoi(result[-1])
+        ltt = torch.tensor(last_token)
+        ltt = ltt.unsqueeze(0)
+        with torch.no_grad():
+            prob_distribution = model(ltt)
+            prob_distribution = torch.nn.functional.softmax(prob_distribution, dim=-1)
+        prob_distribution = torch.squeeze(prob_distribution) / torch.sum(prob_distribution)
+        sampled_token = np.random.choice([i for i in range(VOCAB_SIZE)], p=prob_distribution.numpy())  # TODO: multiple sampling and beam search or nucleus sampling
+        result += itoc(sampled_token)
+
+    print("Result:")
+    print(result)