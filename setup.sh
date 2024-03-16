301889cbb59f0bbee8e2a3a777e052e3da3ac427
Shawn Jain <shawnjain.08@gmail.com>
2024-03-15 23:38:26 -0700

    fixed setup.sh for lambdalabs

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ git switch -c jains-cleanroom
 
 pip install torch numpy transformers datasets tiktoken wandb tqdm
 pip install --upgrade torch
+pip install --upgrade torch numpy transformers datasets tiktoken wandb tqdm networkx
 python data/shakespeare_char/prepare.py
 python train.py config/train_shakespeare_char.py
 # python train.py config/train_shakespeare_char.py --eval_iters=20 --log_interval=1 --block_size=64 --batch_size=12 --n_layer=4 --n_head=4 --n_embd=128 --max_iters=2000 --lr_decay_iters=2000 --dropout=0.0
